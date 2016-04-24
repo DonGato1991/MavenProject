ec03e03bb7f9eaab0c8aa5afda15441c1f25750e
Juan Caspi <juanitocaspi@gmail.com>
2016-04-24 17:22:06 -0500

    fourth test

--- MavenEnterpriseApp-web/src/main/java/ListNews.java
+++ MavenEnterpriseApp-web/src/main/java/ListNews.java
@@ -40,6 +40,7 @@ public class ListNews extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ListNews at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ListNews at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
